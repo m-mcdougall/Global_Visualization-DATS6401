0d6e4cae70dc7f42cb060a71a6abe3e457a5751a
m-mcdougall <39218258+m-mcdougall@users.noreply.github.com>
2021-02-19 19:15:59 -0500

    Supplimental Education
    Load and integrate the supplemental education database to fill gaps in worldbank dataset.

--- Data_Processing.py
+++ Data_Processing.py
@@ -16,7 +16,55 @@ os.chdir(wd)
 
 #%%
 
-#Import all data
+#The education dataset from Worldbank is missing many values in the timeframe we're looking at
+#We'll load a supplimental dataset from OECD, and fill in the missing values
+# The OECD Dataset itself is too small to be used on its own.
+
+
+def process_education(df_in):
+    
+    
+    df_in=df_in.drop(['INDICATOR', 'SUBJECT', 'MEASURE', 'FREQUENCY', 'Flag Codes'], axis=1)
+    df_in['TIME']=df_in['TIME'].astype(str)
+    df_in=df_in.pivot(index='LOCATION', columns='TIME', values='Value')
+    df_in.index.name='Country Code'
+    
+    return df_in
+        
+
+#Load and process the supplimental dataframe
+supplimental=pd.read_csv(os.path.abspath(wd+'//Data//DP_LIVE_19022021234709194.csv'))
+supplimental=process_education(supplimental)
+
+
+#Load the main education dataframe
+edu=os.path.abspath(wd+'//Data//API_SE.XPD.TOTL.GD.ZS_DS2_en_csv_v2_1926663.csv', )
+edu=pd.read_csv(edu, skiprows=[0,1,2])
+
+#Set education index to countrycode, for merging
+edu=edu.set_index('Country Code')
+
+#Get the intersection of the two daaframes for filling
+edu_fill=edu.index.intersection(supplimental.index)
+edu_fill=edu.loc[edu_fill,:]
+
+#If value is na in the main dataframe, get the missing value from the matching index in the supplimental
+edu_fill=edu_fill.fillna(supplimental)
+
+#Merge the filled in dataframe back into the original
+edu=edu_fill.combine_first(edu)
+
+#Reformat to match the other dataframes
+edu=edu.reset_index()
+edu=edu.set_index('Country Name')
+
+
+del supplimental, edu_fill
+
+
+#%%
+
+#Import the rest of the data
 #Will need additional pre-processing
 #Education is currently expressed as a percentage of GDP, will need to be converted.
 
@@ -29,31 +77,116 @@ gdp=os.path.abspath(wd+'//Data//API_MS.MIL.XPND.CD_DS2_en_csv_v2_1928211.csv')
 gdp=pd.read_csv(gdp, skiprows=[0,1,2], index_col='Country Name')
 
 
-
-edu=os.path.abspath(wd+'//Data//API_SE.XPD.TOTL.GD.ZS_DS2_en_csv_v2_1926663.csv')
-edu=pd.read_csv(edu, skiprows=[0,1,2], index_col='Country Name')
-
-
-
 pop=os.path.abspath(wd+'//Data//API_SP.POP.TOTL_DS2_en_csv_v2_1976634.csv')
 pop=pd.read_csv(pop, skiprows=[0,1,2], index_col='Country Name')
 
 
+
+#Education is currently expressed as a % of gdp
+#Convert it to Current USD for uniformity
+
+for col in range(1960, 2020):
+    edu[str(col)] = gdp[str(col)]*edu[str(col)]/100
+
+
+#Now all financial measures are in USD.
+del col
+#%%
 #Collect data
 data={'MIL':mil, 'GDP':gdp, 'EDU':edu, 'POP':pop}
 
 for key in data:
     data[key]=data[key].drop(['Country Code', 'Indicator Name', 'Indicator Code', '2020', 'Unnamed: 65'], axis=1)
 
-df_in=data['MIL']
 
-x=(df_in.shape[0]-(df_in.isnull().sum()))
+#%%
+    
+def plot_percent_non_null(df_in):
+    """
+    Plots the percentage of data present in the dataset
+    
+    df_in: The Dataframe of years of data
+    """
+    
+    x=((df_in.shape[0]-(df_in.isnull().sum()))/df_in.shape[0])*100
+    
+    fig, ax = plt.subplots(figsize=[9,3])
+    ax.plot(range(int(df_in.columns[0]),int(df_in.columns[-1])+1), x)
+    plt.ylabel('% Non-null Datapoints')
+    plt.xlabel('Year')
+    plt.title(f'% Data Present - {key}')
 
-fig, ax = plt.subplots(figsize=[9,3])
-ax.plot(range(1960,2020), x)
-#ax.plot(x)
+    plt.show()
 
-#plt.xticks(rotation=45)
-#ax.set_xticklabels(ax.get_xticklabels(),rotation=45, horizontalalignment='right')
-plt.ylabel('# Non-null Datapoints')
-plt.xlabel('Year')
+for key in data:
+        plot_percent_non_null(data[key])
+    
+#%%
+
+for key in data:
+    data[key]=data[key].filter([str(i) for i in range(2010,2016)], axis=1)
+    plot_percent_non_null(data[key])
+#%%
+
+
+
+def drop_non_reporters(df_in, max_percent_missing=0.6):
+    """
+    Drops countries from dataset if they have more than a given percent 
+    of their data missing
+    
+    df_in: The Dataframe of years of data
+    max_percent_missing: The maximum percent of missing data that is acceptable
+    """
+    
+    #Find number of missing datapoints for each country
+    years=df_in.shape[1]
+    missing_count=df_in.isna().sum(axis=1)
+    
+    #Filter out countries whose missing datapoints exceed the maximum
+    keep=missing_count<years*max_percent_missing
+    
+    print(f'\n---------------------')
+    print(f'\n The Following Countries have been Removed:\n\n{df_in[~keep].index.values}')
+    print(f'\n---------------------')
+    return df_in[keep]
+
+   
+
+
+for key in data:
+    data[key]=drop_non_reporters(data[key], max_percent_missing=0.8)
+    plot_percent_non_null(data[key])
+
+
+
+
+#%%
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 Government Expenditure on Education, total (% of GDP) - [https://data.worldbank.org/indicator/SE.XPD.TOTL.GD.ZS]
 Global Population Data - [https://data.worldbank.org/indicator/SP.POP.TOTL]
 Military Expenditure Data - [https://data.worldbank.org/indicator/MS.MIL.XPND.CD]
-GDP Data - [https://data.worldbank.org/indicator/NY.GDP.MKTP.CD], 
+GDP Data - [https://data.worldbank.org/indicator/NY.GDP.MKTP.CD],
+
+
+Education - [https://data.oecd.org/eduresource/public-spending-on-education.htm#indicator-chart], Public spending on education Primary to tertiary, % of GDP, 1995 – 2016
+OECD (2021), Public spending on education (indicator). doi: 10.1787/f99b45d0-en (Accessed on 19 February 2021)
